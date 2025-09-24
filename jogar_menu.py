a1ea09438a1bce847f714b1ef415ac397ab2a4e6
Viniciusdionizio <vinihbs123@gmail.com>
2025-09-24 11:22:22 -0300

    Update jogar_menu.py

--- jogar_menu.py
+++ jogar_menu.py
@@ -1,10 +1,9 @@
-import curses
+import curses,time
 from biblioteca import biblioteca_br,biblioteca_eng
 
 
-def curses_input(stdscr, prompt="",pos=[10,10]): #input no curses
-	stdscr.clear()
-	stdscr.addstr(pos[0],pos[1],prompt)
+def curses_input(stdscr, prompt="",pos=(10,10)): #input no curses
+	stdscr.addstr(0,0,prompt)
 	stdscr.refresh()
 	curses.echo()
 	s = stdscr.getstr(pos[0], pos[1]+len(prompt))
@@ -22,7 +21,7 @@ def jogar_menu(stdscr):
 	curses.init_pair(2, curses.COLOR_WHITE,curses.COLOR_YELLOW)
 	curses.init_pair(3, curses.COLOR_WHITE,curses.COLOR_GREEN)
     
-	def erro(palavra):
+	def letras(palavra):
 		dicc = {}
 		for i in palavra:
 			dicc[i] = 0
@@ -30,43 +29,53 @@ def jogar_menu(stdscr):
 			dicc[i] += 1
 		return dicc
 
-	def letra_letra(palavra,p):
-		lista_final=[]
-		c = erro(p)
-		lista = [*range(len(p))]
-		for letra in palavra:
-			for i in lista:
-				if palavra[i] == p[i] and palavra[i] == letra:
-					lista_final.append(palavra[i])
-					c[palavra[i]] -= 1
-					lista.remove(i)
-					break
-		for i in lista:
-			if palavra[i] in p and c[palavra[i]] > 0:
-				lista_final.append(palavra[i])
-				c[palavra[i]] -= 1
-			else:
-				lista_final.append(palavra[i])
-		return lista_final
-
 	def printar_palavra(stdscr):
-		palavra = biblioteca_br()
+		palavra = 'furar'
 		contador = 0
 
 		while True:
+			dicc=letras(palavra)
 			if contador > 5:
-				stdscr.addstr('Derrota')
+				stdscr.addstr(y,x-4,'DERROTA!')
+				stdscr.refresh()
+				stdscr.getch()
 				break
 			contador +=1
-			a=curses_input(stdscr,"",[10,10])
-			l = letra_letra(a, palavra)
-			for i in range(len(l)):
-				if l[i] == palavra[i]:
-					stdscr.addstr(y,x+i,f'{l[i]}',curses.color_pair(3))
-				elif l[i] != palavra[i]:
-					stdscr.addstr(y,x+1,f'{l[i]}',curses.color_pair(1))
+			while True:  # repete até o jogador digitar 5 letras
+				a = curses_input(stdscr, "", ((y-10)+contador*3, x-5//2))
+				if len(a) == 5:
+					break
+				else:
+					stdscr.addstr(3, x-(27//2),"DIGITE EXATAMENTE 5 LETRAS!", curses.color_pair(1))
+					stdscr.refresh()
+					time.sleep(1.5)
+					stdscr.move(3, x-(27//2))   # limpa aviso
+					stdscr.clrtoeol()
+					stdscr.move((y-10)+contador*3, x-5)
+					stdscr.clrtoeol()
+					stdscr.refresh()
+				
+			for i in range(5):
+				letra=a[i]
+				if letra == palavra[i]:
+					dicc[letra]-=1
+					stdscr.addstr((y-10)+contador*3,x-len(a)//2+i,f'{letra}',curses.color_pair(3))
+				stdscr.refresh()
+			for j in range(5):
+				letra=a[j]
+				if letra != palavra[j]:
+					if letra not in dicc.keys() or dicc[letra]==0:
+						stdscr.addstr((y-10)+contador*3,x-len(a)//2+j,f'{letra}',curses.color_pair(1))
+					else: 
+						stdscr.addstr((y-10)+contador*3,x-len(a)//2+j,f'{letra}',curses.color_pair(2))
+						dicc[letra]-=1
+			stdscr.refresh()
+				
 			if a == palavra:
-				stdscr.clear()
+				stdscr.addstr(3,x-7,'VITÓRIA!!',curses.color_pair(3))
+				stdscr.refresh()
+				time.sleep(3)
 				break
+
 	printar_palavra(stdscr)
 curses.wrapper(jogar_menu)
