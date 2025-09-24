e108fb368e946e7302786d8778ffe57673ceb5cb
JVinicius530 <jvinicius896@gmail.com>
2025-09-24 19:42:32 -0300

    Update Rankeada.py

--- Rankeada.py
+++ Rankeada.py
@@ -3,3 +3,17 @@ from ranking import rank
 def organiza(lista):
   n = True
   for i in range(len(lista)-1):
+    if lista[i][1]<lista[i+1][1]:
+      lista[i], lista[i+1] = lista[i+1], lista[i]
+      n = False
+  return n
+
+lis = []
+
+for e in rank.items():
+  lis.append(e)
+
+while True:
+  if organiza(lis):
+    break
+
